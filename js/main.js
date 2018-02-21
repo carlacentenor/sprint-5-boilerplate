d2a5d944f016b5f4e039a10cdffa5f0acd0bf521
carlacentenor <carlacentenor@gmail.com>
2018-02-20 23:08:41 -0500

    mostrar mensajes

---
+++ js/main.js
@@ -0,0 +1,86 @@
+
+const search = $('#search');
+const container = $('#container');
+const userTopic = $('#user');
+const messageTopic = $('#message');
+const btnCreateTopic = $('#btn-create-topic');
+
+
+function template(author,content,id,responses){
+  template1 = `<div class=col-12><div class=" card mt-3 title" data-id=${id} >
+    <div class="card-header">
+    <div class="d-inline-block mr-3"><img src="assets/images/usuario.png" ></div><div class="d-inline-block"><h5>${author}</h5></div>
+     
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Tema:</h5>
+      <a href="views/verTopic.html" class="card-text respons-topic content-post-topic" data-id=${id}>${content}</a>
+      <div>
+      <p   class="">Respuestas : ${responses}</p>
+      </div>
+    </div>
+  </div>
+  </div>`;
+  container.prepend(template1);
+  
+}
+
+
+function handleResponse(data) {
+  data.forEach(function (element) {
+
+    let author= element.author_name;
+    let content = element.content;
+    let id = element.id;
+    let responses = element.responses_count
+   template(author,content,id,responses)
+
+  })
+}
+
+
+
+
+$.ajax({
+  url: `http://examen-laboratoria-sprint-5.herokuapp.com/topics`
+}).done(handleResponse);
+
+
+function createTheme() {
+  let user = userTopic.val();
+  let content = messageTopic.val();
+  let data = {
+    "author_name": user,
+    "content": content
+  }
+
+  let url = 'http://examen-laboratoria-sprint-5.herokuapp.com/topics';
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    success: function(){
+      template(user,content,0,0);
+    },
+
+  });
+
+  
+}
+
+
+
+
+$(document).on('click', '.respons-topic', function() {
+  let value = $(this).data('id');
+ localStorage.id = value;
+
+});
+
+
+
+// filtrar
+
+
+btnCreateTopic.on('click',createTheme);
--- js/verTopic.js
+++ js/verTopic.js
@@ -1,6 +1,89 @@
-var topicId = getParameterByName('topic_id');
+let userTopic = $('.user-topic');
+let message = $('.content-topic');
+let userMessage = $('#user-message');
+let messagePost = $('#message-post');
+let btnCreateMessage = $('#btn-create-message');
+let contMessage = $('.container-message');
 
-//Solo por propositos de debug
-if(topicId){
-  alert("El topic ID es:"+topicId);
+let id = localStorage.id;
+
+function getInfo(){
+  let url = `http://examen-laboratoria-sprint-5.herokuapp.com/topics/${id}`;
+  $.ajax({
+    url: url
+  }).done(showInfo);
+  
+  
 }
+
+function getMessage(){
+  let url = `http://examen-laboratoria-sprint-5.herokuapp.com/topics/${id}/responses`;
+  $.ajax({
+    url: url
+  }).done(showMessage);
+}
+
+
+
+function showInfo(data){
+userTopic.text (data.author_name);
+message.text (data.content);
+}
+
+function showMessage(data){
+  data.forEach(function (element) {
+
+    let author= element.author_name;
+    let content = element.content;
+ 
+   templateMessage(author,content)
+
+  })
+}
+
+function templateMessage(author,content){
+  template = `<div class="card mt-3">
+    <div class="card-header">
+     <h5>${author}</h5>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Tema:</h5>
+      <p class="card-text">${content}</p>
+    
+    </div>
+  </div>`;
+  contMessage.prepend(template);
+  
+}
+
+
+function createMessage() {
+  let user = userMessage.val();
+  let content = messagePost.val();
+  let data = {
+    "author_name": user,
+    "content": content,
+    "topic_id":id
+  }
+
+  let url =   `http://examen-laboratoria-sprint-5.herokuapp.com/topics/${id}/responses`;
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    success: function(){
+      templateMessage(user,content)
+    },
+
+  });
+
+  
+}
+
+
+
+btnCreateMessage.on('click',createMessage);
+
+getInfo();
+getMessage()
